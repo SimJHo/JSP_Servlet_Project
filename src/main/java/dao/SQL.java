1bd4962aaa2a6f9c85c8cd414943ed5b2f6ea915
YANGEUNOK\82103 <tlawnsgh1@naver.com>
2022-12-12 00:18:13 +0900

    fourth commit

--- src/main/java/dao/SQL.java
+++ src/main/java/dao/SQL.java
@@ -4,7 +4,7 @@ public class SQL {
 	protected String candidate = "SELECT M_NO, "
 			+ "M_NAME, "
 			+ "P_NAME, "
-			+ "CASE P_SCHOOL WHEN '1' THEN '고졸' WHEN '2' THEN '학사' WHEN '3' THEN '석사' WHEN '4' THEN '박사' END AS P_SCHOOL, "
+			+ "DECODE(P_SCHOOL, '1', '고졸', '2', '학사', '3', '석사', '4', '박사') AS P_SCHOOL, "
 			+ "(SUBSTR(M_JUMIN, 1,6)||'-'||SUBSTR(M_JUMIN, 7, 13)) AS M_JUMIN, "
 			+ "M_CITY, "
 			+ "P_TEL1, "
@@ -18,13 +18,12 @@ public class SQL {
 	
 	protected String voteCheck = "SELECT V_NAME, "
 			+ "(19||SUBSTR(V_JUMIN, 1,2)||'년'||SUBSTR(V_JUMIN, 3, 2)||'월'||SUBSTR(V_JUMIN, 5, 2)||'일생')AS V_JUMIN, "
-			+ "'만 '||TRUNC(MONTHS_BETWEEN(TRUNC(SYSDATE),"
-			+ " TO_DATE(19||(SUBSTR(V_JUMIN,1,6)),'YYYYMMDD')) / 12)||'세' AS V_AGE, "
-			+ "CASE SUBSTR(V_JUMIN,7,1) WHEN '1' THEN '남' WHEN '2' THEN '여' END AS V_GENDER, "
+			+ "'만 '||TRUNC(MONTHS_BETWEEN(TRUNC(SYSDATE), TO_DATE(19||(SUBSTR(V_JUMIN,1,6)),'YYYYMMDD')) / 12)||'세' AS V_AGE, "
+			+ "DECODE(SUBSTR(V_JUMIN,7,1), '1', '남', '2', '여') AS V_GENDER, "
 			+ "M_NO, "
 			+ "V_TIME, "
-			+ "CASE V_CONFIRM WHEN 'Y' THEN '확인' WHEN 'N' THEN '미확인' END AS V_CONFIRM "
-			+ "from tbl_vote";
+			+ "DECODE(V_CONFIRM, 'Y', '확인', 'N', '미확인') AS V_CONFIRM "
+			+ "FROM TBL_VOTE";
 	
 	protected String voteCount = "SELECT  M.M_NO, "
 			+ "M.M_NAME, "
@@ -64,13 +63,15 @@ public class SQL {
 			+ "TEACHER_NAME, "
 			+ "CLASS_NAME, "
 			+ "TO_CHAR(CLASS_PRICE, 'L999,999') AS CLASS_PRICE, "
-			+ "SUBSTR(TEACHER_REGIST_DATE, 1, 4)||'년'||SUBSTR(TEACHER_REGIST_DATE, 5, 2)||'월'||SUBSTR(TEACHER_REGIST_DATE, 7, 2)||'일' AS TEACHER_REGIST_DATE "
+			+ "SUBSTR(TEACHER_REGIST_DATE, 1, 4)||'년'||SUBSTR(TEACHER_REGIST_DATE, 5, 2)||'월'"
+			+ "||SUBSTR(TEACHER_REGIST_DATE, 7, 2)||'일' AS TEACHER_REGIST_DATE "
 			+ "FROM TBL_TEACHER_202201";
 	
 	protected String list2 = "SELECT SUBSTR(REGIST_MONTH, 1, 4)||'년'||SUBSTR(REGIST_MONTH, 5, 2)||'월' AS REGIST_MONTH, "
 			+ "C.C_NO, "
 			+ "M.C_NAME, "
-			+ "CASE C.TEACHER_CODE WHEN '100' THEN '초급반' WHEN '200' THEN '중급반' WHEN '300' THEN '고급반' WHEN '400' THEN '심화반' END AS TEACHER_CODE, "
+			+ "CASE C.TEACHER_CODE WHEN '100' THEN '초급반' WHEN '200' THEN '중급반' WHEN '300' THEN '고급반' "
+			+ "WHEN '400' THEN '심화반' END AS TEACHER_CODE, "
 			+ "C.CLASS_AREA, "
 			+ "TO_CHAR(C.TUITION, 'L999,999') AS TUITION, "
 			+ "M.GRADE "
@@ -84,6 +85,7 @@ public class SQL {
 			+ "CLASS_NAME, "
 			+ "TEACHER_NAME, "
 			+ "TO_CHAR(TUITION , 'L999,999') AS TUITION "
-			+ "FROM TBL_TEACHER_202201 T, (SELECT TEACHER_CODE, SUM(TUITION) AS TUITION FROM TBL_CLASS_202201 GROUP BY TEACHER_CODE) C "
+			+ "FROM TBL_TEACHER_202201 T, (SELECT TEACHER_CODE, SUM(TUITION) AS TUITION "
+			+ "FROM TBL_CLASS_202201 GROUP BY TEACHER_CODE) C "
 			+ "WHERE T.TEACHER_CODE = C.TEACHER_CODE";
 }
